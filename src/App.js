40096ebe2d49e03fa9103b6dc9a5f2b236890bbb
Aleksandr Solntsev <san2008nas@gmail.com>
2022-11-08 09:39:21 +0200

    allGood

--- src/App.js
+++ src/App.js
@@ -1,41 +1,36 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-
-import HeaderContainer from "./components/Header/HeaderContainer";
+import Header from "./components/Header/Header";
 import Login from "./components/Login/Login";
 import ShopForm from "./components/Products/ShopForm";
-
-
+import PrivateRoute from "./PrivateRoute";
 import { isAuth } from "./Redux/profileReducer";
 
-
 function App() {
-  const dispatch=useDispatch()
-  useEffect (() => {
-    dispatch(isAuth())
-    
+  const dispatch = useDispatch();
+  const isLoading = useSelector((state) => state.profile.isLoading);
+
+  useEffect(() => {
+    dispatch(isAuth());
   }, []);
-  const auth = useSelector ((state) => state.profile.isAuth)
-  
 
   return (
     <div className="app-wrapper">
-      <HeaderContainer/>
-      
-      {!auth &&<Login />}
-      
-      {auth && <ShopForm />}
-      
-
-      {/* <Routes>  
-        <Route path="/login" element={<Login />} />
-        <Route path="/shop" element={<ShopForm />} />
-        <Route path="/profile" element={<Profile />} />
-      </Routes> */}
-
-      
+      <Header />
+      {isLoading ? (
+        <p>Loading....</p>
+      ) : (
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route element={<PrivateRoute />}>
+            <Route path="/shop" element={<ShopForm />} />
+          </Route>
+          <Route path="*" element={<p>There's nothing here: 404!</p>} />
+        </Routes>
+      )}
     </div>
   );
 }
--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,13 +1,10 @@
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit'
-import authReducer from './authReducer'
-import productsReducer from './productsReducer'
-import profileReducer from './profileReducer'
-
+import { configureStore } from "@reduxjs/toolkit";
+import productsReducer from "./productsReducer";
+import profileReducer from "./profileReducer";
 
 export default configureStore({
   reducer: {
-    auth: authReducer,
     products: productsReducer,
-    profile: profileReducer
+    profile: profileReducer,
   },
-})
+});
